0b21838c546313d685dd7ab6b2199fb9aa7bc2fd
ncgears <1918ncgears@gmail.com>
2025-09-14 02:37:14 -0400

    Replace var with explicit types for java 8/11 compatibility

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Vision_AprilTags_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Vision_AprilTags_Test.java
@@ -1,25 +1,33 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.vision.AprilTagVision;
 import org.firstinspires.ftc.teamcode.util.TelemetryUtil;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+
+
+import java.util.List;
+
 
 @TeleOp(name = "Vision_AprilTags_Test", group = "Vision")
 public class Vision_AprilTags_Test extends OpMode {
     private AprilTagVision vision;
     private TelemetryUtil T;
 
+
     @Override public void init() {
         T = new TelemetryUtil(this);
         vision = new AprilTagVision(hardwareMap);
     }
 
+
     @Override public void loop() {
-        var d = vision.atag.getDetections();
+        List<AprilTagDetection> d = vision.atag.getDetections();
         T.t(3, "Detections", d.size());
         if (!d.isEmpty()) {
-            var first = d.get(0);
+            AprilTagDetection first = d.get(0);
             T.t(4, "id", first.id);
             T.t(4, "center", String.format("(%.1f, %.1f)", first.center.x, first.center.y));
             if (first.metadata != null) {
