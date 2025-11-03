34d73bdc27e853104b5926718b002d6cc08a2271
ncgears <1918ncgears@gmail.com>
2025-11-02 23:05:09 -0500

    Update for FTCLib

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
@@ -1,49 +1,42 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import org.firstinspires.ftc.teamcode.constants.Constants;
 
-/** Basic mecanum drive: robot-centric power mapping + helpers. */
+/** FTCLib-backed drive using Motor wrappers and MecanumDrive. */
 public class DriveSubsystem {
-    private final DcMotorEx fl, fr, rl, rr;
+    private final Motor fl, fr, rl, rr;
+    private final MecanumDrive mecanum;
+
     public DriveSubsystem(HardwareMap hw) {
-        fl = hw.get(DcMotorEx.class, Constants.Drive.FL);
-        fr = hw.get(DcMotorEx.class, Constants.Drive.FR);
-        rl = hw.get(DcMotorEx.class, Constants.Drive.RL);
-        rr = hw.get(DcMotorEx.class, Constants.Drive.RR);
+        fl = new Motor(hw, Constants.Drive.FL);
+        fr = new Motor(hw, Constants.Drive.FR);
+        rl = new Motor(hw, Constants.Drive.RL);
+        rr = new Motor(hw, Constants.Drive.RR);
 
-        fl.setDirection(Constants.Drive.INVERT_FL ? DcMotor.Direction.REVERSE : DcMotor.Direction.FORWARD);
-        fr.setDirection(Constants.Drive.INVERT_FR ? DcMotor.Direction.REVERSE : DcMotor.Direction.FORWARD);
-        rl.setDirection(Constants.Drive.INVERT_RL ? DcMotor.Direction.REVERSE : DcMotor.Direction.FORWARD);
-        rr.setDirection(Constants.Drive.INVERT_RR ? DcMotor.Direction.REVERSE : DcMotor.Direction.FORWARD);
+        fl.setInverted(Constants.Drive.INVERT_FL);
+        fr.setInverted(Constants.Drive.INVERT_FR);
+        rl.setInverted(Constants.Drive.INVERT_RL);
+        rr.setInverted(Constants.Drive.INVERT_RR);
 
-        if (Constants.Drive.RUN_WITHOUT_ENCODERS) {
-            fl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            fr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            rl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            rr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        }
-        DcMotor.ZeroPowerBehavior z = Constants.Drive.ZERO_POWER_BRAKE
-                ? DcMotor.ZeroPowerBehavior.BRAKE : DcMotor.ZeroPowerBehavior.FLOAT;
+        fl.setRunMode(Motor.RunMode.RawPower);
+        fr.setRunMode(Motor.RunMode.RawPower);
+        rl.setRunMode(Motor.RunMode.RawPower);
+        rr.setRunMode(Motor.RunMode.RawPower);
+
+        Motor.ZeroPowerBehavior z = Constants.Drive.ZERO_POWER_BRAKE
+                ? Motor.ZeroPowerBehavior.BRAKE : Motor.ZeroPowerBehavior.FLOAT;
         fl.setZeroPowerBehavior(z); fr.setZeroPowerBehavior(z); rl.setZeroPowerBehavior(z); rr.setZeroPowerBehavior(z);
 
-        for (LynxModule hub : hw.getAll(LynxModule.class)) {
-            hub.setBulkCachingMode(Constants.Drive.REV_BULK_CACHE_AUTO
-                    ? LynxModule.BulkCachingMode.AUTO : LynxModule.BulkCachingMode.OFF);
-        }
+        mecanum = new MecanumDrive(fl, fr, rl, rr);
     }
-    /** Robot-centric drive mapping (x: +right, y: +forward, rot: +CCW). */
+
+    /** Robot-centric command: x=+right, y=+forward, rot=+CCW. */
     public void driveRobotCentric(double x, double y, double rot) {
-        double flp = y + x + rot;
-        double frp = y - x - rot;
-        double rlp = y - x + rot;
-        double rrp = y + x - rot;
-        double max = Math.max(1.0, Math.max(Math.abs(flp),
-                Math.max(Math.abs(frp), Math.max(Math.abs(rlp), Math.abs(rrp)))));
-        fl.setPower(flp / max); fr.setPower(frp / max); rl.setPower(rlp / max); rr.setPower(rrp / max);
+        mecanum.driveRobotCentric(x, y, rot);
     }
-    public void stop() { fl.setPower(0); fr.setPower(0); rl.setPower(0); rr.setPower(0); }
+    public void stop() { mecanum.stop(); }
 }
