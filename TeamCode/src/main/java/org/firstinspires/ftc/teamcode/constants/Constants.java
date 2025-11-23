ad5645716f20d9489ae79766696258b0d8bed2ab
ncgears <1918ncgears@gmail.com>
2025-11-23 13:57:29 -0500

    Enable tuning opmodes from global constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/Constants.java
@@ -291,7 +291,8 @@ public final class Constants {
 
     /** Global Constants. */
     public static final class Global {
-        public static final boolean ENABLE_DASHBOARD = true;
+        public static final boolean ENABLE_DASHBOARD = true; //enable "Dashboard Enable/Disable" opmode
+        public static final boolean ENABLE_TUNING = true; //enable all the tuning opmodes
     }
 
     private Constants() {}
